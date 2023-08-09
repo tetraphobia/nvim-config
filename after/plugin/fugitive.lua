e1fb5c0f80a508b082153b72b9ae0ec4b97a5917
Jacob Teddy <tetraphobia@wyvrn.dev>
2023-08-09 17:01:25 -0400

    drop: unused fugitive command

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -3,7 +3,3 @@ vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
 vim.keymap.set('n', '<leader>p', function()
     vim.cmd.Git('push')
 end)
-
-vim.keymap.set('n', '<leader>P', function()
-    vim.cmd.Git({ 'pull', '--rebase' })
-end)
