727f5b91375b2074ed22e12e2b9b4dd3b297d881
Jacob Teddy <tetraphobia@wyvrn.dev>
2023-08-09 16:57:35 -0400

    refactor: include remap in init.lua

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,9 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+
+vim.keymap.set('n', '<leader>p', function()
+    vim.cmd.Git('push')
+end)
+
+vim.keymap.set('n', '<leader>P', function()
+    vim.cmd.Git({ 'pull', '--rebase' })
+end)
--- lua/tetraphobia/init.lua
+++ lua/tetraphobia/init.lua
@@ -1,1 +1,2 @@
 require("tetraphobia.config")
+require("tetraphobia.remap")
