021ab675df59ade15f60a876cd449d42dcf1bd20
Jacob Teddy <tetraphobia@wyvrn.dev>
2023-08-21 05:20:27 -0400

    fix: show hidden folders when using telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', function()
+    builtin.find_files({ hidden = true })
+end)
 vim.keymap.set('n', '<leader>fg', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
