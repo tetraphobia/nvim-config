17c32a882611843cce5175ce6789d7a36a369d42
Jacob Teddy <tetraphobia@wyvrn.dev>
2023-11-01 11:22:24 -0400

    style: formatting

--- lua/tetraphobia/bootstrap.lua
+++ lua/tetraphobia/bootstrap.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -17,9 +17,9 @@ return require('packer').startup(function(use)
 
     -- Telescope
     use {
-      'nvim-telescope/telescope.nvim',
-      tag = '0.1.1',
-      requires = 'nvim-lua/plenary.nvim'
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        requires = 'nvim-lua/plenary.nvim'
     }
 
     -- Colorscheme
@@ -38,7 +38,7 @@ return require('packer').startup(function(use)
     }
 
     -- Vim Fugitive
-    use 'tpope/vim-fugitive' 
+    use 'tpope/vim-fugitive'
 
     -- LSP
     use {
