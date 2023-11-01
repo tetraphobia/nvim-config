aa4c89783c9b18880f8834dd1eea722e4e1a18e9
Jacob Teddy <tetraphobia@wyvrn.dev>
2023-11-01 11:29:44 -0400

    fix: add fallback shell for terminals

--- lua/tetraphobia/terminal.lua
+++ lua/tetraphobia/terminal.lua
@@ -1,4 +1,11 @@
-local SHELL = "zsh"
+function zsh_or_fallback()
+    local sh = io.open('/bin/zsh', 'r')
+    if sh~= nil then
+        io.close(sh) return "zsh" else return "bash"
+    end
+end
+
+local SHELL = zsh_or_fallback()
 
 vim.keymap.set('n', '<leader>t', function()
     vim.cmd.edit('term://' .. SHELL)
