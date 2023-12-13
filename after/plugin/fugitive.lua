d75dd373d63b8351f23f0a06bd20fc385f2b9441
Jacob Teddy <tetraphobia@wyvrn.dev>
2023-12-13 14:05:57 -0500

    feat: additional git bindings

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,13 @@
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gg', vim.cmd.Git)
 
-vim.keymap.set('n', '<leader>p', function()
+vim.keymap.set('n', '<leader>gp', function()
+    vim.cmd.Git('pull')
+end)
+
+vim.keymap.set('n', '<leader>gP', function()
     vim.cmd.Git('push')
+end)
+
+vim.keymap.set('n', '<leader>gs', function()
+    vim.cmd.Git('status')
 end)
