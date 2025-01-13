8e0cbf75e97f2f1d76ed0ade2f20241da22004d3
Jacob Teddy <tetraphobia@wyvrn.dev>
2025-01-12 23:17:15 -0500

    fix: toggle symbols and diags individually

--- lua/config/plugins/trouble.lua
+++ lua/config/plugins/trouble.lua
@@ -11,12 +11,24 @@ return {
 				auto_open = false,
 				open_no_results = true,
 				warn_no_results = false,
-				preview = {
-					type = "split",
-					relative = "win",
-					position = "right",
-					size = 0.3,
-				},
+				-- Bottom pane
+				-- preview = {
+				-- 	type = "split",
+				-- 	relative = "win",
+				-- 	position = "right",
+				-- 	size = 0.3,
+				-- },
+                -- Floating pane
+                preview = {
+                    type = "float",
+                    relative = "editor",
+                    border = "rounded",
+                    title = "Diagnostics",
+                    title_pos = "center",
+                    position = "center",
+                    size = { width = 0.4, height = 0.4 },
+                    zindex = 200,
+                }
 			},
 		},
 	},
