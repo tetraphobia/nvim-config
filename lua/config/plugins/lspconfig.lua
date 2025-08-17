5408e691b3ab731392d361bada53c93689a30e3e
Jacob Teddy <tetraphobia@wyvrn.dev>
2025-08-17 18:50:50 -0400

    chore: update lazy packages

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -9,38 +9,46 @@ return {
 	config = function()
 		local lspconfig = require("lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local mason_lspconfig = require("mason-lspconfig")
 
-		mason_lspconfig.setup_handlers({
-			function(server_name)
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-            ["clangd"] = function()
-                lspconfig.clangd.setup({
-                    capabilities = capabilities,
-                    cmd = {
-                        "clangd",
-                        "--fallback-style=webkit"
-                    }
-                })
-            end,
-			["lua_ls"] = function()
-				lspconfig.lua_ls.setup({
-					capabilities = capabilities,
-					diagnostics = {
-						underline = true,
-					},
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				})
-			end,
+		lspconfig.basedpyright.setup({
+            capabilities = capabilities,
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "openFilesOnly",
+				useLibraryCodeForTypes = true,
+			},
 		})
+
+		-- mason_lspconfig.setup_handlers({
+		-- 	function(server_name)
+		-- 		require("lspconfig")[server_name].setup({
+		-- 			capabilities = capabilities,
+		-- 		})
+		-- 	end,
+		--           ["clangd"] = function()
+		--               lspconfig.clangd.setup({
+		--                   capabilities = capabilities,
+		--                   cmd = {
+		--                       "clangd",
+		--                       "--fallback-style=webkit"
+		--                   }
+		--               })
+		--           end,
+		-- 	["lua_ls"] = function()
+		-- 		lspconfig.lua_ls.setup({
+		-- 			capabilities = capabilities,
+		-- 			diagnostics = {
+		-- 				underline = true,
+		-- 			},
+		-- 			settings = {
+		-- 				Lua = {
+		-- 					diagnostics = {
+		-- 						globals = { "vim" },
+		-- 					},
+		-- 				},
+		-- 			},
+		-- 		})
+		-- 	end,
+		-- })
 	end,
 }
